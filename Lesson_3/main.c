8c18ffa01615645074cecacf63edda3900fafcbb
Alessio Desktop <alessio.mungelli@edu.unito.it>
2020-08-06 18:04:14 +0200

    Exercises did during the lessons

--- Lesson_3/main.c
+++ Lesson_3/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
+INPUT - what the problem needs to be solved
+OUTPUT - what the problem returns us (result)
+*/
 int main(int argc, char const *argv[]) {
-  /* code */
+  //printf("Hello World!");
+  float a = 5;
+  float b = 7;
+  int c = 7;
+  printf("%f %d \n", a+b, c);
+  printf("%f\n", a-b);
+  printf("%f\n", a*b);
+  printf("%f\n", a/b);
   return 0;
 }
---
+++ Lesson_4/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  //declaration
+  int a;
+  //initialization
+  a = 5;
+  float b = 6.4;
+  printf("%d\n",a);
+  printf("%f\n",b);
+  return 0;
+}
