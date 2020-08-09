6abcbdfadeeb69a71e06c6c34b8cf62dfcafcdde
Alessio Desktop <alessio.mungelli@edu.unito.it>
2020-08-09 15:06:02 +0200

    Basic example

---
+++ Lesson_5/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  /*A variable is a memory location
+    that can store only ONE VALUE
+  */
+  //declaration
+  int a;
+  float b;
+  char c;
+  //initialization
+  a = 7;
+  b = 7.9;
+  c = 'x';
+  printf("Hello World!\n");
+  printf("%d\n",a);
+  printf("%f\n",b);
+  printf("%c\n",c);
+  return 0;
+}
